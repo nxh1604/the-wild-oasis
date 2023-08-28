924ec3106d541afe3ec66974a0d888140d82c5a4
GoldenAzusa <101515269+nxh1604@users.noreply.github.com>
2023-08-28 16:06:36 +0700

    building bookings pages

--- src/pages/Bookings/Bookings.tsx
+++ src/pages/Bookings/Bookings.tsx
@@ -1,12 +1,20 @@
+import BookingTable from "../../features/bookings/BookingTable";
+import BookingTableOperations from "../../features/bookings/BookingTableOperations";
 import Heading from "../../ui/Heading";
 import Row from "../../ui/Row";
+import { StyledContainer } from "../Cabins/Cabins";
 
 function Bookings() {
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All bookings</Heading>
-      <p>TEST</p>
-    </Row>
+    <StyledContainer>
+      <Row type="horizontal">
+        <Heading as="h1">All bookings</Heading>
+        <BookingTableOperations />
+      </Row>
+      <Row type="vertical">
+        <BookingTable />
+      </Row>
+    </StyledContainer>
   );
 }
 
