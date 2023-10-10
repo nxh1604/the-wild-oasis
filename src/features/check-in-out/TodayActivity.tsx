dbed80b2bc9ad3aac64ded24d50f77e69c717aae
GoldenAzusa <101515269+nxh1604@users.noreply.github.com>
2023-10-10 20:57:10 +0700

    finished last feature

--- src/features/check-in-out/TodayActivity.tsx
+++ src/features/check-in-out/TodayActivity.tsx
@@ -11,7 +11,7 @@ const StyledToday = styled.div`
   background-color: var(--color-grey-0);
   border: 1px solid var(--color-grey-100);
   border-radius: var(--border-radius-md);
-
+  min-width: max-content;
   padding: 3.2rem;
   display: flex;
   flex-direction: column;
